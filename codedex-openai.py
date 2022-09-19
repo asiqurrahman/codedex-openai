a8a434b433deac5347a9f574872cd17a14c3ad3f
asiqurrahman <asiqurrahman13@gmail.com>
2022-09-19 16:14:52 -0400

    increased paragraph length

--- codedex-openai.py
+++ codedex-openai.py
@@ -5,12 +5,12 @@ import openai
 # Set Api Key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def generate_blog(blog_topic):
+def generate_blog(paragraph_topic):
     # Makes a http request and returns an object
     response = openai.Completion.create(
         model="text-davinci-002",
-        prompt="Write a paragraph about the following topic." + " " + blog_topic,
-        max_tokens=300,
+        prompt="Write a paragraph about the following topic." + " " + paragraph_topic,
+        max_tokens=400,
         temperature=0.3,
     )
     
@@ -25,7 +25,7 @@ keep_writing = True
 while keep_writing:
   write_paragraph = input('Write a paragraph? Y for yes, anything else for no')
   if(write_paragraph == "Y"):
-    blog_topic = input("What should this paragraph talk about?")
-    print(generate_blog(blog_topic))
+    paragraph_topic = input("What should this paragraph talk about?")
+    print(generate_blog(paragraph_topic))
   else:
     keep_writing = False
